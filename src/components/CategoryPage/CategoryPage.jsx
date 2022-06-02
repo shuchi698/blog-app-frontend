c2cdf2e594c62e7d0426d14d1d741e0d0d2c04fc
shuchi698 <75218993+shuchi698@users.noreply.github.com>
2022-06-02 16:32:34 +0530

    Update CategoryPage.jsx

--- src/components/CategoryPage/CategoryPage.jsx
+++ src/components/CategoryPage/CategoryPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import BlogData from '../BlogData';
+import { Link, useSearchParams } from 'react-router-dom';
+// import BlogData from '../BlogData';
 import LatestArticlesComponent from '../Home/LatestArticles/LatestArticlesComponent';
 import TopPostComponent from '../Home/LatestArticles/TopPostComponent';
 import './CategoryPage.css';
+import axios from 'axios';
+import SpinnerComp from './../Spinner/SpinnerComp';
 
 window.onbeforeunload = () => {
     window.scrollTo(0, 0);
@@ -11,59 +13,82 @@ window.onbeforeunload = () => {
 
 const CategoryPage = () => {
 
-    const [postNumber, setPostNumber] = useState(5);
-    const { category } = useParams();
-    const [blog, setBlog] = useState(null);
+    const [ queryParams ] = useSearchParams();
+    const name = queryParams.get('name');
 
+
+    const [postNumber, setPostNumber] = useState(5);
+
+    const [blogData, setBlogData] = useState([]);
+
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        let blog = BlogData.find(blog => blog.category === category)
-        if (blog) {
-            setBlog(blog);
-        }
-    }, [category]);
+
+        axios.get(`https://sheltered-fjord-87205.herokuapp.com/api/category/?name=${name}`)
+            .then(response => {
+                setBlogData(response.data.data);
+                setLoading(true)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }, [name]);
 
     return (
-        <div className='CategoryPage'>
+        <>
             {
-                blog ? <div>
-                    <div className='LatestArticles'>
-                        <div className='latest-articles-heading'>{blog.category}</div>
-                        <div className='latest-articles-box'>
-                            <div className='latest-articles-left'>
-                                {
-                                    BlogData.filter((item) => item.category === blog.category).slice(0, postNumber).map((data, index) => (
-                                        <LatestArticlesComponent data={data} key={index} />
-                                    ))
-                                }
-                                <div className='load-more' onClick={() => setPostNumber(postNumber + 2)}>
-                                    <i className="fas fa-arrow-down"></i>LOAD MORE
-                                </div>
-                            </div>
-                            <div className='latest-articles-right'>
-                                <div className='top-post-heading'>Top Posts</div>
-                                <div className='top-post-flexbox'>
-                                    <img src={BlogData.filter((item) => item.category === blog.category)[1].img} alt="top-post" />
-                                    <Link to={`/${BlogData.filter((item) => item.category === blog.category)[1].category}/${BlogData.filter((item) => item.category === blog.category)[1].id}`}>
-                                        <div className='top-post-title'>{BlogData.filter((item) => item.category === blog.category)[1].title}</div>
-                                    </Link>
-                                    <div className='top-post-details'>
-                                        <span className='top-post-type'>{BlogData.filter((item) => item.category === blog.category)[1].category} </span>
-                                        <span className='top-post-date'>/ {BlogData.filter((item) => item.category === blog.category)[1].date}</span>
-                                    </div>
+                loading ?
+                <div className='CategoryPage'>
+                {
+                    blogData ? <div>
+                        <div className='LatestArticles'>
+                            <div className='latest-articles-heading'>{name}</div>
+                            <div className='latest-articles-box'>
+                                <div className='latest-articles-left'>
                                     {
-                                        BlogData.filter((item) => item.category === blog.category).slice(4, 7).map((data, index) => (
-                                            <TopPostComponent data={data} key={index}/>
+                                        blogData.slice(0, postNumber).map((data, index) => (
+                                            <LatestArticlesComponent data={data} key={index} />
                                         ))
                                     }
+                                    <div className='load-more' onClick={() => setPostNumber(postNumber + 2)}>
+                                        <i className="fas fa-arrow-down"></i>LOAD MORE
+                                    </div>
                                 </div>
-                                <div className='advertisement-box'>Advertisement</div>
+                                <div className='latest-articles-right'>
+                                    <div className='top-post-heading'>Top Posts</div>
+                                    <div className='top-post-flexbox'>
+                                        {
+                                            blogData.slice(1, 2).map((data, index) => (
+                                                <div key={index} >
+                                                    <img src={data.img} alt="top-post" />
+                                                    <Link to={`/category/${data.category}/${data.id}`}>
+                                                        <div className='top-post-title'>{data.title}</div>
+                                                    </Link>
+                                                    <div className='top-post-details'>
+                                                        <span className='top-post-type'>{data.category} </span>
+                                                        <span className='top-post-date'>/ {data.date}</span>
+                                                    </div>
+                                                </div>
+                                            ))
+                                        }
+
+                                        {
+                                            blogData.slice(4, 7).map((data, index) => (
+                                                <TopPostComponent data={data} key={index}/>
+                                            ))
+                                        }
+                                    </div>
+                                    <div className='advertisement-box'>Advertisement</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div> : null
+                    </div> : null
+                }
+            </div> : <SpinnerComp />
             }
-        </div>
+        </>
     );
 }
 

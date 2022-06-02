20a33460fbb7afbbf344f3fb3f03ae219212054c
shuchi698 <75218993+shuchi698@users.noreply.github.com>
2022-06-02 16:38:11 +0530

    Update Intro.jsx

--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import BlogData from '../../BlogData';
+// import BlogData from '../../BlogData';
 import IntroComponent from './IntroComponent';
 import './intro.css';
 
 
-const Intro = () => {
-
+const Intro = ({ blogData }) => {
     return (
-        <div className='Intro'>
+     <div className='Intro'>
             <div className='intro-box'>
                 {
-                    BlogData.filter((item) => item.id === 0).map((data, index) => (
+                    blogData.filter((item) => item.id === 0).map((data, index) => (
                         <IntroComponent data={data} classStyle='classStyle1' key={index} />
                     ))
                 }
                 {
-                    BlogData.slice(1, 3).map((data, index) => (
+                    blogData.slice(1, 3).map((data, index) => (
                         <IntroComponent data={data} classStyle='classStyle2' key={index} />
                     ))
                 }

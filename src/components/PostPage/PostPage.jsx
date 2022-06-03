2e6a3e5754d0077cf39c0cc2088048c392eb988d
shuchi698 <75218993+shuchi698@users.noreply.github.com>
2022-06-03 08:04:14 +0530

    Update PostPage.jsx

--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -1,67 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import BlogData from '../BlogData';
 import AuthorDetailsComponent from './AuthorDetailsComponent';
 import RelatedReadComponent from './RelatedReadComponent';
 import './PostPage.css';
+import axios from 'axios';
+import SpinnerComp from '../Spinner/SpinnerComp';
 
 const PostPage = () => {
+    const { category, id } = useParams();
+    const [blogPost, setBlogPost] = useState({});
+    const [blogAuth, setBlogAuth] = useState([]);
+    const [blogTags, setBlogTags] = useState([]);
 
-    const { id } = useParams();
-    const [blog, setBlog] = useState(null);
+    const [loading, setLoading] = useState(false);
+
 
     useEffect(() => {
-        let blog = BlogData.find(blog => blog.id === parseInt(id));
-        if (blog) {
-            setBlog(blog);
-        }
-    }, [id]);
+        
+        axios.get(`https://sheltered-fjord-87205.herokuapp.com/api/article/${id}`)
+            .then(response => {
+                setBlogPost(response.data.blogPost);
+                setBlogAuth(response.data.blogAuth);
+                setBlogTags(response.data.blogPost.tags)
+                setLoading(true)
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
+    }, [category, id]);
+
+    
     return (
-        <div className='PostPage'>
+        <>
             {
-                blog ? <div>
-                        <div key={blog.id} className='post-page-box'>
-                            <div className='post-page-title'>
-                                {blog.title}
-                                <div className='author-flexbox'>
-                                    <AuthorDetailsComponent data={blog} classStyle='written-by-hide' />
-                                    <div className='social-btns'>
-                                        <a href="https://twitter.com/i/flow/login" target="blank"><i className="fab fa-twitter-square"></i></a>
-                                        <a href="https://www.facebook.com/login.php/" target="blank"><i className="fab fa-facebook-square"></i></a>
-                                        <a href="https://www.instagram.com/accounts/login/" target="blank"><i className="fab fa-instagram"></i></a>
+                loading ?
+                <div className='PostPage'>
+                {
+                    blogPost ? <div>
+                            <div key={blogPost.id} className='post-page-box'>
+                                <div className='post-page-title'>
+                                    {blogPost.title}
+                                    <div className='author-flexbox'>
+                                        <AuthorDetailsComponent data={blogPost} classStyle='written-by-hide' />
+                                        <div className='social-btns'>
+                                            <a href="https://twitter.com/i/flow/login" target="blank"><i className="fab fa-twitter-square"></i></a>
+                                            <a href="https://www.facebook.com/login.php/" target="blank"><i className="fab fa-facebook-square"></i></a>
+                                            <a href="https://www.instagram.com/accounts/login/" target="blank"><i className="fab fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+                                    <img src={blogPost.img} className="post-page-img" alt="blogPost-img" />
+                                    <div className='post-page-para' dangerouslySetInnerHTML={{__html: blogPost['para'] }}></div>
+                                    <div className='post-page-tags-box'>
+                                        {
+                                            blogTags.map((item, index) => (
+                                                <span key={index}>{item}</span>
+                                            ))
+                                        }
+                                    </div>
+                                   
+                                    <div className='written-by-box'>
+                                        <AuthorDetailsComponent data={blogPost} classStyle='written-by' />
                                     </div>
                                 </div>
-                                <img src={blog.img} className="post-page-img" alt="blog-img" />
-                                <div className='post-page-para' dangerouslySetInnerHTML={{__html: blog['para'] }}></div>
-                                <div className='post-page-tags-box'>
-                                    {
-                                        blog.tags.map((item, index) => (
-                                            <span key={index}>{item}</span>
-                                        ))
-                                    }
-                                </div>
-                                {/* add likes later */}
-                                <div className='written-by-box'>
-                                    <AuthorDetailsComponent data={blog} classStyle='written-by' />
+                            </div>
+                            <div className='related-read-box'>
+                                <div className='related-read-innerbox'>
+                                    <div className='more-from-siren'>More From The Siren</div>
+                                    <div className='related-read-flexbox'>
+                                        {
+                                            blogAuth.map((data, index) => (
+                                                <RelatedReadComponent data={data} key={index}/>
+                                            ))
+                                        }
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='related-read-box'>
-                            <div className='related-read-innerbox'>
-                                <div className='more-from-siren'>More From The Siren</div>
-                                <div className='related-read-flexbox'>
-                                    {
-                                        BlogData.filter((item) => item.author === blog.author).slice(1, 4).map((data, index) => (
-                                            <RelatedReadComponent data={data} key={index}/>
-                                        ))
-                                    }
-                                </div>
-                            </div>
-                        </div>
-                    </div> : null
+                        </div> : null
+                }
+            </div> : <SpinnerComp />
             }
-        </div>
+        </>
     );
 }
 

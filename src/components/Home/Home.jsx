db0b990e8ba6d77bccd61c06f81670b4700f9526
shuchi698 <75218993+shuchi698@users.noreply.github.com>
2022-06-03 07:54:43 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Intro from './Intro/Intro';
 import Latest from './Latest/Latest';
 import LatestArticles from './LatestArticles/LatestArticles';
 import LatestStories from './LatestStories/LatestStories';
+import axios from 'axios';
+import SpinnerComp from '../Spinner/SpinnerComp';
 
 const Home = () => {
+    const [blogData, setBlogData] = useState([]);
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+
+        axios.get("https://sheltered-fjord-87205.herokuapp.com/api/home")
+            .then(response => {
+                setBlogData(response.data.data);
+                setLoading(true)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }, [])
+
     return (
-        <div className='Home'>
-            <Intro />
-            <Latest />
-            <LatestArticles />
-            <LatestStories />
-        </div>
+        <>
+        {
+            loading ?
+            <div className='Home'>
+                    <Intro blogData={blogData} />
+                    <Latest blogData={blogData} />
+                    <LatestArticles blogData={blogData} />
+                    <LatestStories blogData={blogData} />
+            </div>
+            : <SpinnerComp />
+        }
+        </>
     )
 }
 

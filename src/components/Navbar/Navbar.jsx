fa2fd87d7a992b4af2f01ba61e0a09c652751533
shuchi698 <75218993+shuchi698@users.noreply.github.com>
2022-06-03 08:20:56 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
             <div className='nav'>
                 <ul className='nav-menu'>
                     <li><NavLink to="/home" data-hover='Home'>Home</NavLink></li>
-                    <li><NavLink to="/cat/Bollywood" data-hover='Bollywood'>Bollywood</NavLink></li>
-                    <li><NavLink to="/cat/Technology" data-hover='Technology'>Technology</NavLink></li>
-                    <li><NavLink to="/cat/Hollywood" data-hover='Hollywood' >Hollywood</NavLink></li>
-                    <li><NavLink to="/cat/Travel" data-hover='Travel'>Travel</NavLink></li>
-                    <li><NavLink to="/cat/Food" data-hover='Food'>Food</NavLink></li>
+                    <li><NavLink to="/category1/?name=bollywood" data-hover='Bollywood'>Bollywood</NavLink></li>
+                    <li><NavLink to="/category2/?name=technology" data-hover='Technology'>Technology</NavLink></li>
+                    <li><NavLink to="/category3/?name=hollywood" data-hover='Hollywood' >Hollywood</NavLink></li>
+                    <li><NavLink to="/category4/?name=travel" data-hover='Travel'>Travel</NavLink></li>
+                    <li><NavLink to="/category5/?name=food" data-hover='Food'>Food</NavLink></li>
                 </ul>
             </div>
         </div>
